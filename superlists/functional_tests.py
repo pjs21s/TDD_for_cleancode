68e73e3156cebb787c0dacdeef2097929b9939ee
JinSang Park <jsgd1254@gmail.com>
2019-06-01 20:28:37 +0900

    4장까지 한거 업로드

---
+++ superlists/functional_tests.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
+from selenium.webdriver.common.keys import Keys
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\\firefox.exe')
+        self.browser = webdriver.Firefox(firefox_binary=binary)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않음"
+        )
+
+        self.fail('Finish the Test')
+    
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
