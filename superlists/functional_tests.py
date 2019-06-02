5263d0d405f6c23423705885d47d6d60455d28e9
JinSang Park <jsgd1254@gmail.com>
2019-06-02 15:09:09 +0900

    fucntional_tests.py 변경

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import unittest
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -11,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -29,12 +38,24 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
+        WebDriverWait(self.browser, 10).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), '공작깃털 사기'
+            )
+        )
+
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            "신규 작업이 테이블에 표시되지 않음"
-        )
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.cehck_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.cehck_for_row_in_list_table('1: 공작깃털 사기')
 
         self.fail('Finish the Test')
     
