f07d7c7064545f74657bd077f47b1b92d5479d90
JinSang Park <jsgd1254@gmail.com>
2019-06-02 22:41:56 +0900

    5장까지 했음

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,8 +3,12 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
+import re
 
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile('<input[^>]*csrfmiddlewaretoken[^>]*>')
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -13,7 +17,10 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('lists/home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(
+          re.sub(self.pattern_input_csrf, '', response.content.decode()),
+          re.sub(self.pattern_input_csrf, '', expected_html)
+      )
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -22,9 +29,56 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'lists/home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+        self.assertEqual(
+        re.sub(self.pattern_input_csrf, '', response.content.decode()),
+        re.sub(self.pattern_input_csrf, '', expected_html)
+    )
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
